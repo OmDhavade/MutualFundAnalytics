7073152b0b8314abcdd370c04c7d2c7196cd401c
OmDhavade <omdhavade12345@gmail.com>
2026-06-23 22:51:46 +0530

    Initial project setup

---
+++ data_ingestion.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+folder = "data/raw"
+
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        path = os.path.join(folder, file)
+
+        df = pd.read_csv(path)
+
+        print("\n===================")
+        print(file)
+        print("===================")
+
+        print("Shape:", df.shape)
+        print("\nColumns:")
+        print(df.columns)
+
+        print("\nData Types:")
+        print(df.dtypes)
+
+        print("\nFirst 5 Rows:")
+        print(df.head())
